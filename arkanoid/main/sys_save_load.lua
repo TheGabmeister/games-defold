fb2392acfe349d50c5e66b887d01318e7e4db693
TheGabmeister <thegabmeister@protonmail.com>
2025-05-23 22:47:33 +0800

    Fix save system

--- arkanoid/main/sys_save_load.lua
+++ arkanoid/main/sys_save_load.lua
@@ -1,16 +1,5 @@
 local M = {}
 
-local function get_save_path()
-    local app_path = sys.get_application_path()
-    -- Remove trailing slash if present
-    if app_path:sub(-1) == "/" or app_path:sub(-1) == "\\" then
-        app_path = app_path:sub(1, -2)
-    end
-    -- Use OS-specific separator
-    local sep = package.config:sub(1,1)
-    return app_path .. sep .. "highscore.save"
-end
-
 function M.load_highscore()
     local filename = sys.get_save_file("sys_save_load", "highscore")
     local data = sys.load(filename)
@@ -18,7 +7,7 @@ function M.load_highscore()
 end
 
 function M.save_highscore(highscore)
-    local filename = sys.get_save_file("sys_save_load", "highscore")
+    local filename = sys.get_save_file("my_game", "highscore")
     sys.save(filename, { highscore = highscore })
 end
 
