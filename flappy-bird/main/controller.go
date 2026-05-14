c7523324ce3ca9d09477769895f1d00f3e1614a2
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-05-14 09:19:04 +0800

    Add assets

---
+++ flappy-bird/main/controller.go
@@ -0,0 +1,16 @@
+components {
+  id: "main"
+  component: "/main/main.script"
+}
+components {
+  id: "flap"
+  component: "/assets/sounds/flap.sound"
+}
+components {
+  id: "score"
+  component: "/assets/sounds/score.sound"
+}
+components {
+  id: "hit"
+  component: "/assets/sounds/hit.sound"
+}
